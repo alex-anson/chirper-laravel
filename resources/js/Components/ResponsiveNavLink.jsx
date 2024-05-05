d0548568b7c6e1e409b91dcdfed9945e2c8fceb8
Alex Anson <1alex.anson@gmail.com>
2024-05-04 20:23:57 -0400

    add Chirp model, migration, and resource controller. add 2 routes. render chirps page. turned it into dark mode.. *facepalm*

--- resources/js/Components/ResponsiveNavLink.jsx
+++ resources/js/Components/ResponsiveNavLink.jsx
@@ -6,9 +6,9 @@ export default function ResponsiveNavLink({ active = false, className = '', chil
             {...props}
             className={`w-full flex items-start ps-3 pe-4 py-2 border-l-4 ${
                 active
-                    ? 'border-indigo-400 text-indigo-700 bg-indigo-50 focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700'
-                    : 'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300'
-            } text-base font-medium focus:outline-none transition duration-150 ease-in-out ${className}`}
+                    ? 'border-indigo-700 bg-gray-900 text-indigo-50 focus:text-gray-900 focus:bg-indigo-300 focus:border-indigo-700'
+                    : 'border-transparent text-gray-300 hover:text-indigo-100 hover:bg-gray-700 hover:border-gray-300 focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300'
+            } text-base font-medium focus:outline-none transition-colors ${className}`}
         >
             {children}
         </Link>
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -9,20 +9,23 @@ export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
+        <div className="min-h-screen bg-gray-700">
+            <nav className="bg-gray-800 border-b border-gray-900">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-300" />
                                 </Link>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                                <NavLink href={route('chirps.index')} active={route().current('chirps.index')}>
+                                    Chirps
                                 </NavLink>
                             </div>
                         </div>
@@ -95,6 +98,9 @@ export default function Authenticated({ user, header, children }) {
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
                         </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('chirps.index')} active={route().current('chirps.index')} >
+                            Chirps
+                        </ResponsiveNavLink>
                     </div>
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
@@ -114,7 +120,7 @@ export default function Authenticated({ user, header, children }) {
             </nav>
 
             {header && (
-                <header className="bg-white shadow">
+                <header className="bg-gray-900 shadow">
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
                 </header>
             )}
