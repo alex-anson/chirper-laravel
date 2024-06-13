30de66865b2e1123289b0031ca85e93b1842abd0
Alex Anson <1alex.anson@gmail.com>
2024-06-13 17:21:02 -0400

    tutorial - showing chirps

--- database/migrations/2024_05_04_225857_create_chirps_table.php
+++ database/migrations/2024_05_04_225857_create_chirps_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-    // you are here -> https://bootcamp.laravel.com/inertia/showing-chirps#retrieving-the-chirps
 
     /**
      * Reverse the migrations.
